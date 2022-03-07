6d6c6bd90102fe5febbe8f9fb4f23bf1f0af9576
Ignat Bojinov <91905588+Ignat40@users.noreply.github.com>
2022-03-07 11:26:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 class My_math:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, x):
+        self.x = x
+        #self.y = y
+
+    def factoriel(self):
+    
+        fact = 1 
+        for i in range(1, self.x + 1):
+            fact *= i
+        print(fact)
+
+
+
+
+m = My_math(5)
+m.factoriel()
+        
+        
